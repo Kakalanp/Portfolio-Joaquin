290120262ea7009383ffaee9d6fecbebe0c45310
Kakalanp <78335447+Kakalanp@users.noreply.github.com>
2022-01-24 17:05:53 -0500

    Finished the mobile menu
    - Added the close menu icon
    
    - updated gitignore (ESLint)
    
    -updated package.json (ESLint)
    
    -updated CSS file to include the mobile menu styles and changed some minor inconveniences.
    
    -added .JS file, finally, it only includes the mobile menu for now.

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-document.querySelector(".menu").addEventListener("click", function () {
-  var menuContainer = document.querySelector(".menu-container");
-  menuContainer.style.display = "flex";
+document.querySelector('.menu').addEventListener('click', () => {
+  const menuContainer = document.querySelector('.menu-container');
+  menuContainer.style.display = 'flex';
 
-  var closeButton = document.createElement("a");
-  closeButton.classList.add("closeButton");
+  const closeButton = document.createElement('a');
+  closeButton.classList.add('close-button');
 
-  var items = document.querySelectorAll(".menu-container a");
-  var itemsArr = Array.from(items);
+  const items = document.querySelectorAll('.menu-container a');
+  const itemsArr = Array.from(items);
   itemsArr.unshift(closeButton);
+  menuContainer.innerHTML = '';
 
-  itemsArr.map(function (el) {});
+  itemsArr.map((el) => {
+    menuContainer.appendChild(el);
+    return null;
+  });
+
+  menuContainer.addEventListener('click', () => {
+    menuContainer.style.display = 'none';
+    closeButton.remove();
+  });
 });
