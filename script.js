1c6f942ea56eaae9935fabd2f0db93c073330e8d
Behnam1369 <behnam.aghaali@yahoo.com>
2022-01-25 20:26:07 +0330

    Add css styles for popup window

--- script.js
+++ script.js
@@ -21,33 +21,63 @@ document.querySelector('.menu').addEventListener('click', () => {
   });
 });
 
-const button = document.querySelectorAll('.card .button')
+const button = document.querySelectorAll('.card .button');
 
 const buttonArr = Array.from(button);
 
 buttonArr.map((el) => {
   el.addEventListener('click', () => {
-    const popup =  document.createElement('div');
+    const popupContainer = document.createElement('div');
+    popupContainer.classList.add('popup-container');
+
+    const popup = document.createElement('div');
+    popup.classList.add('popup');
+
+    popupContainer.appendChild(popup);
+
     const popupObj = {};
     popupObj.img = '/Images/ProjectPopup.svg';
     popupObj.title = 'Keeping track of hundreds of components';
-    popupObj.tags = ['HTML','CSS','JavaScript','Ruby on rails'];
-    popupObj.paragraph = 'lorem ipsum'
+    popupObj.tags = ['HTML', 'CSS', 'JavaScript', 'Ruby on rails'];
+    popupObj.paragraph = 'lorem ipsum';
+
+    const closeButton = document.createElement('span');
+    popup.appendChild(closeButton);
 
     const img = document.createElement('img');
     img.setAttribute('src', popupObj.img);
+
+    const seeProjectButton = document.createElement('a');
+    seeProjectButton.classList.add('button');
+    seeProjectButton.classList.add('see-project-button');
+    seeProjectButton.innerHTML = 'See Project';
+
+    const seeSourceButton = document.createElement('a');
+    seeSourceButton.classList.add('button');
+    seeSourceButton.classList.add('see-source-button');
+    seeSourceButton.innerHTML = 'See Source';
+
     const title = document.createElement('h2');
     title.innerHTML = popupObj.title;
-    const tags = document.createElement('ul')
+    const tags = document.createElement('ul');
     popupObj.tags.map((t) => {
       const tag = document.createElement('li');
       tag.innerHTML = t;
       tags.appendChild(tag);
-    })
+    });
+
+    const paragraph = document.createElement('p');
+    paragraph.innerHTML = popupObj.paragraph;
 
     popup.appendChild(img);
     popup.appendChild(title);
+    popup.appendChild(seeProjectButton);
+    popup.appendChild(seeSourceButton);
     popup.appendChild(tags);
-    console.log(popup);
-  })
-})
+    popup.appendChild(paragraph);
+
+    document.querySelector('body').appendChild(popupContainer);
+
+    //console.log(popup);
+  });
+});
