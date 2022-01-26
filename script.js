9b3685cf4f0e969c46eab14f58d1bf156350d897
Kakalanp <78335447+Kakalanp@users.noreply.github.com>
2022-01-26 12:58:57 -0500

    Added dynamic info to cards and popup window
    -Renamed "projectpopup.svg" to "ProjectScreenshot1.svg".
    
    -Added "ProjectScreenshot2.svg".
    
    -Removed static cards from HTML.
    
    -Removed unused tags in CSS.
    
    -Changed/added styles in CSS.
    
    -Added project info list in JS.
    
    -Using project info list from JS to show info on HTML cards and popup window.
    
    -Fixed linter errors.

--- script.js
+++ script.js
@@ -1,3 +1,78 @@
+const projects = [
+  {
+    img: 'Images/ProjectScreenshot1.svg',
+    title: 'Multi-Post Stories Gain+Glory',
+    tags: ['HTML', 'CSS'],
+    live: '',
+    source: '',
+    paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+  },
+  {
+    img: 'Images/ProjectScreenshot2.svg',
+    title: 'Multi-Post Stories Gain+Glory',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    live: '',
+    source: '',
+    paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+  },
+  {
+    img: 'Images/placeholder.png',
+    title: 'Multi-Post Stories Gain+Glory',
+    tags: ['HTML', 'CSS'],
+    live: '',
+    source: '',
+    paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+  },
+  {
+    img: 'Images/placeholder.png',
+    title: 'Multi-Post Stories Gain+Glory',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    live: '',
+    source: '',
+    paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+  },
+  {
+    img: 'Images/placeholder.png',
+    title: 'Multi-Post Stories Gain+Glory',
+    tags: ['HTML', 'CSS', 'JavaScript', 'Ruby on rails'],
+    live: '',
+    source: '',
+    paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+  },
+  {
+    img: 'Images/placeholder.png',
+    title: 'Multi-Post Stories Gain+Glory',
+    tags: ['HTML', 'CSS', 'Ruby on rails'],
+    live: '',
+    source: '',
+    paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
+  },
+];
+
+function createworks() {
+  const works = document.querySelector('div.works');
+  projects.map((el, i) => {
+    const work = document.createElement('div');
+    work.classList.add('card');
+
+    const div1 = document.createElement('div');
+    div1.classList.add('screenshot');
+    div1.innerHTML = `<img src ="${el.img}" alt = "project ${i}" />`;
+
+    const div2 = document.createElement('div');
+    div2.classList.add('info');
+    div2.innerHTML = `<h3 class="text">${el.title}</h3> <ul class="languages">${el.tags.map((t) => `<li>${t}</li>`)
+      .join('')}</ul> <a class="button"  >See project</a>`;
+
+    work.appendChild(div1);
+    work.appendChild(div2);
+
+    works.appendChild(work);
+
+    return null;
+  });
+}
+
 document.querySelector('.menu').addEventListener('click', () => {
   const menuContainer = document.querySelector('.menu-container');
   menuContainer.style.display = 'flex';
@@ -21,76 +96,80 @@ document.querySelector('.menu').addEventListener('click', () => {
   });
 });
 
-const button = document.querySelectorAll('.card .button');
+function createpopups() {
+  const button = document.querySelectorAll('.card .button');
+  const buttonArr = Array.from(button);
+  buttonArr.map((el, i) => {
+    el.addEventListener('click', () => {
+      const popupContainer = document.createElement('div');
+      popupContainer.classList.add('popup-container');
 
-const buttonArr = Array.from(button);
+      const popup = document.createElement('div');
+      popup.classList.add('popup');
 
-buttonArr.map((el) => {
-  el.addEventListener('click', () => {
-    const popupContainer = document.createElement('div');
-    popupContainer.classList.add('popup-container');
+      popupContainer.appendChild(popup);
 
-    const popup = document.createElement('div');
-    popup.classList.add('popup');
+      const closeButton = document.createElement('a');
+      closeButton.classList.add('close-button');
+      closeButton.addEventListener('click', () => {
+        popupContainer.remove();
+      });
+      popup.appendChild(closeButton);
 
-    popupContainer.appendChild(popup);
+      const img = document.createElement('img');
+      img.setAttribute('src', projects[i].img);
+      popup.appendChild(img);
 
-    const popupObj = {};
-    popupObj.img = '/Images/ProjectPopup.svg';
-    popupObj.title = 'Keeping track of hundreds of components';
-    popupObj.tags = ['HTML', 'CSS', 'JavaScript', 'Ruby on rails'];
-    popupObj.paragraph = 'lorem ipsum dolor sit amet, consectetur adipiscing ';
+      const info = document.createElement('div');
+      info.classList.add('flexrow');
 
-    const closeButton = document.createElement('a');
-    closeButton.addEventListener('click', () =>{
-      popupContainer.remove();
-    })
-    popup.appendChild(closeButton);
+      const txtinfo = document.createElement('div');
+      txtinfo.classList.add('flexcolumn');
 
-    const img = document.createElement('img');
-    img.setAttribute('src', popupObj.img);
+      const title = document.createElement('h2');
+      title.innerHTML = projects[i].title;
+      txtinfo.appendChild(title);
 
-    const info = document.createElement('div');
-    info.classList.add('flexrow')
-    const txtinfo = document.createElement('div');
-    txtinfo.classList.add('flexcolumn')
+      const tags = document.createElement('ul');
+      tags.classList.add('languages');
+      projects[i].tags.map((t) => {
+        const tag = document.createElement('li');
+        tag.innerHTML = t;
+        tags.appendChild(tag);
+        return null;
+      });
+      txtinfo.appendChild(tags);
 
-    const title = document.createElement('h2');
-    title.innerHTML = popupObj.title;
+      const seeProjectButton = document.createElement('a');
+      seeProjectButton.classList.add('button');
+      seeProjectButton.classList.add('see-project-button');
+      seeProjectButton.innerHTML = 'See Project';
+      info.appendChild(seeProjectButton);
 
-    const tags = document.createElement('ul');
-    tags.classList.add('languages');
-    popupObj.tags.map((t) => {
-      const tag = document.createElement('li');
-      tag.innerHTML = t;
-      tags.appendChild(tag);
+      const seeSourceButton = document.createElement('a');
+      seeSourceButton.classList.add('button');
+      seeSourceButton.classList.add('see-source-button');
+      seeSourceButton.innerHTML = 'See Source';
+      info.appendChild(seeSourceButton);
+
+      const paragraph = document.createElement('p');
+      paragraph.innerHTML = projects[i].paragraph;
+      info.appendChild(paragraph);
+
+      info.appendChild(txtinfo);
+      info.appendChild(seeProjectButton);
+      info.appendChild(seeSourceButton);
+      info.appendChild(paragraph);
+
+      popup.appendChild(info);
+
+      document.querySelector('body').appendChild(popupContainer);
     });
-
-    const seeProjectButton = document.createElement('a');
-    seeProjectButton.classList.add('button');
-    seeProjectButton.classList.add('see-project-button');
-    seeProjectButton.innerHTML = 'See Project';
-
-    const seeSourceButton = document.createElement('a');
-    seeSourceButton.classList.add('button');
-    seeSourceButton.classList.add('see-source-button');
-    seeSourceButton.innerHTML = 'See Source';
-
-    const paragraph = document.createElement('p');
-    paragraph.innerHTML = popupObj.paragraph;
-
-    popup.appendChild(img);
-
-    txtinfo.appendChild(title);
-    txtinfo.appendChild(tags);
-
-    info.appendChild(txtinfo);
-    info.appendChild(seeProjectButton);
-    info.appendChild(seeSourceButton);
-    info.appendChild(paragraph);
-
-    popup.appendChild(info);
-
-    document.querySelector('body').appendChild(popupContainer);
+    return null;
   });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  createworks();
+  createpopups();
 });
